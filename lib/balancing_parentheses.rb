61a952a82b5f135e2f7f478cb4011bbd916da4b2
Rebecca Sissman <rsissman15@aol.com>
2023-03-30 12:04:38 -0400

    commit tree a7811b274ccda4e2a32cc890d1605d2e85a0c719

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(s)
+    for(i=0;i<s.length;i++){
+        let char=stack[@stack.length-1]
+        if(s[i]=="(" || s[i]=="{" || s[i]=="["){
+            @stack.push(s[i])
+        }
+    elseif((char=="(" && s[i]==")")||
+    (char=="{" && s[i]=="}")||
+    (char =="[" && s[i]=="]")){
+        @stack.pop()
+    }else{
+        return false
+    }
+}
+end
